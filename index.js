3d74201b2b0935757060b193e606e97582d4afb6
andrsand <angorko@list.ru>
2023-05-12 19:05:08 +0300

    An example of using component props

--- index.js
+++ index.js
@@ -1,16 +1,7 @@
-const rootApp = document.getElementById("app"); // элемент для рендеринга приложения React
-// получаем корневой элемент 
-const root = ReactDOM.createRoot(rootApp);
-function tick() {
-  root.render( /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h1", null, "\u0421\u0435\u0440\u0432\u0438\u0441 \u043F\u043E\u043B\u0443\u0447\u0435\u043D\u0438\u044F \u0432\u0440\u0435\u043C\u0435\u043D\u0438"), /*#__PURE__*/React.createElement("h2", null, "\u0422\u0435\u043A\u0443\u0449\u0435\u0435 \u0432\u0440\u0435\u043C\u044F ", new Date().toLocaleTimeString(), ".")) // элемент, который мы хотим создать
-  );
+function Hello(props) {
+  return /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("p", null, "\u0418\u043C\u044F: ", props.name), /*#__PURE__*/React.createElement("p", null, "\u0412\u043E\u0437\u0440\u0430\u0441\u0442: ", props.age));
 }
-
-setInterval(tick, 1000);
-// рендеринг в корневой элемент
-
-const rootContainer = document.getElementById("container"); // элемент для рендеринга приложения React
-// получаем корневой элемент 
-const root_1 = ReactDOM.createRoot(rootContainer);
-// рендеринг в корневой элемент
-root_1.render( /*#__PURE__*/React.createElement("div", null, /*#__PURE__*/React.createElement("h2", null, "\u041D\u0430\u0447\u0430\u043B\u044C\u043D\u043E\u0435 \u0432\u0440\u0435\u043C\u044F ", new Date().toLocaleTimeString(), ".")));
+ReactDOM.createRoot(document.getElementById("app")).render( /*#__PURE__*/React.createElement(Hello, {
+  name: "Tom",
+  age: "33"
+}));
--- index.jsx
+++ index.jsx
@@ -1,24 +1,12 @@
-const rootApp = document.getElementById("app");    // элемент для рендеринга приложения React
-// получаем корневой элемент 
-const root = ReactDOM.createRoot(rootApp);
-function tick() {
-    root.render(
-        <div>
-            <h1>Сервис получения времени</h1>
-            <h2>Текущее время {new Date().toLocaleTimeString()}.</h2>
-        </div> // элемент, который мы хотим создать
-    );
+function Hello(props) {
+    return <div>
+        <p>Имя: {props.name}</p>
+        <p>Возраст: {props.age}</p>
+    </div>;
 }
-setInterval(tick, 1000);
-// рендеринг в корневой элемент
-
-
-const rootContainer = document.getElementById("container");    // элемент для рендеринга приложения React
-// получаем корневой элемент 
-const root_1 = ReactDOM.createRoot(rootContainer);
-// рендеринг в корневой элемент
-root_1.render(
-    <div>
-        <h2>Начальное время {new Date().toLocaleTimeString()}.</h2>
-    </div>
-);
+ReactDOM.createRoot(
+    document.getElementById("app")
+)
+    .render(
+        <Hello name="Tom" age="33" />
+    );
