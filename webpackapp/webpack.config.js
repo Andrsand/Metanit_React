94d3ee8969f6691ebda97cf0096a3ef96d54a22d
andrsand <angorko@list.ru>
2023-05-13 08:21:26 +0300

    Adding comments to webpackconfig.js

--- webpackapp/webpack.config.js
+++ webpackapp/webpack.config.js
@@ -1,20 +1,20 @@
 const path = require("path");
 
 module.exports = {
-    mode: "development",
+    mode: "development",  // опция mode. "development" указывает, что приложение находится в состоянии разработки.
     entry: "./app/app.jsx", // входная точка - исходный файл
     output: {
         path: path.resolve(__dirname, "./public"),     // путь к каталогу выходных файлов - папка public
         publicPath: "/public/",
-        filename: "bundle.js"       // название создаваемого файла
+        filename: "bundle.js"       // название создаваемого файла. файл, который будет подключаться на веб-страницу
     },
-    devServer: {
-        historyApiFallback: true,
-        static: {
-            directory: path.join(__dirname, "/"),
+    devServer: { // конфигурация запускаемого веб-сервера
+        historyApiFallback: true, // указывает, что будет использоваться HTML5 History API
+        static: {                 // устанавает настройки для статических файлов
+            directory: path.join(__dirname, "/"), // directory указывает, где находится index.html
         },
-        port: 8081,
-        open: true
+        port: 8081, // веб-сервер будет запускаться на порту 8081
+        open: true  // при запуске веб-сервера приложение будет автоматически открываться в веб-браузере
     },
     module: {
         rules: [   //загрузчик для jsx
